7d2f125177f7a20d6f3eadc7de97f40eb966ef6c
Artem <artem.kolomiiets@farbenmeer.de>
2025-02-25 16:46:05 +0100

    feat: Add size data and dynamic text to Sizebutton component

--- src/Sizebutton.tsx
+++ src/Sizebutton.tsx
@@ -1,10 +1,27 @@
-export default function Sizebutton() {
+const data = {
+    name: "blaues T-Shirt",
+    price: 19.99,
+    sizes: ["S", "M", "L", "XL", "XXL"],
+    comments: [
+        {
+            userName: "Max Mustermann",
+            userImage: "https://randomuser.me/api/portraits",
+        },
+        {
+            userName: "Maxi Musterfrau",
+            userImage: "https://randomuser.me/api/portraits",
+        }
+    ]
+}
+
+
+export default function Sizebutton({ text }: { text: string }) {
     return (
         <div>
             <a href="#">
                 <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2">
                     <h2 className="font-bold font-sans">
-                        XS
+                        {text}
                     </h2>
                 </button>
             </a>

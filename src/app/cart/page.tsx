1d11d96641fade71b2c9424c6485e2361f0c11be
Artem <artem.kolomiiets@farbenmeer.de>
2025-02-26 16:16:01 +0100

    feat: Update navigation links to point to the cart and add cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,53 @@
+export default function Home() {
+  return (
+    <div className="text-white">
+      <div>
+        <h1>Cart</h1>
+      </div>
+
+
+      <div>
+        <div>
+          <div>
+            <div>
+              <img src="#" alt="Photo" />
+            </div>
+            <div>
+              <p>Text</p>
+            </div>
+            <div>
+              <p>Preis</p>
+            </div>
+          </div> //Flex
+          <div>
+            <div>
+              <p>Remove</p>
+            </div>
+            <div>
+              <div>
+                <p>Update</p>
+              </div>
+              <div>
+                <p>Addbutton</p>
+              </div>
+            </div> //Flex
+          </div> //Flex
+        </div>
+
+        <div>
+          <div>
+            <div>
+              <p>Total</p>
+            </div>
+            <div>
+              <p>Preis</p>
+            </div>
+          </div> // Flex
+          <div>
+            <p>Checkout</p>
+          </div>
+        </div> //Flex
+      </div> //grid
+    </div>
+  )
+}
